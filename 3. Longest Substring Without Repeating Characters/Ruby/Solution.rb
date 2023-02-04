77f8e6592bfbe003a5fde56174e3d47105cf5d97
Shalaahuddien Daffa <77540526+Shalaahuddien@users.noreply.github.com>
2023-02-04 09:20:28 +0700

    Solution.rb

--- 3. Longest Substring Without Repeating Characters/Ruby/Solution.rb
+++ 3. Longest Substring Without Repeating Characters/Ruby/Solution.rb
@@ -1,36 +1,16 @@
-# Definition for singly-linked list.
-# class ListNode
-#     attr_accessor :val, :next
-#     def initialize(val = 0, _next = nil)
-#         @val = val
-#         @next = _next
-#     end
-# end
-
-# @param {ListNode} l1
-# @param {ListNode} l2
-# @return {ListNode}
-def add_two_numbers(l1, l2)
-    dummy = ListNode.new(0)
-    current = dummy
-    carry = 0
-    
-    while l1 || l2 || carry != 0
-        sum = carry
-        if l1
-            sum += l1.val
-            l1 = l1.next
+def length_of_longest_substring(s)
+    n = s.length
+    set = Set.new
+    max_length, i, j = 0, 0, 0
+    while i < n && j < n
+        if !set.include?(s[j])
+            set.add(s[j])
+            j += 1
+            max_length = [max_length, j - i].max
+        else
+            set.delete(s[i])
+            i += 1
         end
-        if l2
-            sum += l2.val
-            l2 = l2.next
-        end
-        
-        carry = sum / 10
-        sum = sum % 10
-        current.next = ListNode.new(sum)
-        current = current.next
     end
-    
-    dummy.next
+    return max_length
 end
