fbc2d5f77c87b41543329c7216c8acbf7ff4c534
Shalaahuddien Daffa <77540526+Shalaahuddien@users.noreply.github.com>
2023-02-04 09:18:51 +0700

    Solution.js

--- 3. Longest Substring Without Repeating Characters/JavaScript/Solution.js
+++ 3. Longest Substring Without Repeating Characters/JavaScript/Solution.js
@@ -1,36 +1,14 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} l1
- * @param {ListNode} l2
- * @return {ListNode}
- */
-var addTwoNumbers = function(l1, l2) {
-    let dummy = new ListNode(0);
-    let current = dummy;
-    let carry = 0;
-    
-    while (l1 !== null || l2 !== null || carry !== 0) {
-        let sum = carry;
-        if (l1 !== null) {
-            sum += l1.val;
-            l1 = l1.next;
+var lengthOfLongestSubstring = function(s) {
+    let n = s.length;
+    let set = new Set();
+    let maxLength = 0, i = 0, j = 0;
+    while (i < n && j < n) {
+        if (!set.has(s.charAt(j))) {
+            set.add(s.charAt(j++));
+            maxLength = Math.max(maxLength, j - i);
+        } else {
+            set.delete(s.charAt(i++));
         }
-        if (l2 !== null) {
-            sum += l2.val;
-            l2 = l2.next;
-        }
-        
-        carry = Math.floor(sum / 10);
-        sum = sum % 10;
-        current.next = new ListNode(sum);
-        current = current.next;
     }
-    
-    return dummy.next;
+    return maxLength;
 };
