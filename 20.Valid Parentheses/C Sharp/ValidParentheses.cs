c279145a970a38413573f3a212b96c12d154a847
gmurad97 <murad.dev@bk.ru>
2023-02-06 23:45:39 +0400

    Add ValidParentheses Problem Solving

--- 20.Valid Parentheses/C Sharp/ValidParentheses.cs
+++ 20.Valid Parentheses/C Sharp/ValidParentheses.cs
@@ -1,22 +1,34 @@
-            Stack<char> stackBracket = new Stack<char>();
-            foreach(char bracketChar in s)
-            {
-                switch (bracketChar)
+bool IsValid(string s)
+{
+    Stack<char> stackBracket = new Stack<char>();
+    foreach (char bracketChar in s)
+    {
+        switch (bracketChar)
+        {
+            case '(':
+            case '[':
+            case '{':
+                stackBracket.Push(bracketChar);
+                break;
+            case ')':
+                if (stackBracket.Count == 0 || stackBracket.Pop() != '(')
                 {
-                    case '(':
-                    case '[':
-                    case '{':
-                        stackBracket.Push(bracketChar);
-                        break;
-                    case ')':
-                        break;
-                    case ']':
-
-                        break;
-                    case '}':
-
-                        break;
-
+                    return false;
                 }
-            }
-            return false;
+                break;
+            case ']':
+                if (stackBracket.Count == 0 || stackBracket.Pop() != '[')
+                {
+                    return false;
+                }
+                break;
+            case '}':
+                if (stackBracket.Count == 0 || stackBracket.Pop() != '{')
+                {
+                    return false;
+                }
+                break;
+        }
+    }
+    return stackBracket.Count == 0;
+}
